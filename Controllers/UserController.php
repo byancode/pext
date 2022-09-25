d72ed326910ef7d57895cb342116b097fdde2d62
Byancode <byancode@gmail.com>
2022-09-24 23:09:03 -0500

    other chaNGES

---
+++ Controllers/UserController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pext\Controllers;
+
+class UserController
+{
+    public function __construct(
+    ) {}
+}
--- Pages/index.php
+++ Pages/index.php
@@ -16,19 +16,43 @@ return new Pext\Layouts\App(
         children: Div([
             Div($_SERVER['HTTP_REQUEST_URL'] ?? '/', style: 'border: 1px solid red'),
             # los futuros crean un Alpine data con functiones y variables que son generados al compilar el elemento futuro y agregado en el contexto para ser llamado desde el snapshot
-            Div::future(
-                initial: [],
-                promise: route('users.index'),
+            Future::render(
+                promise: [UserController::class, 'index'],
                 builder: function($context, $snapshot) {
+                    $children = Div(style: 'border: 1px solid blue');
+                    foreach ($snapshot->data as $user) {
+                        $children->append(
+                            new UserItem($user),
+                        );
+                    }
+
                     return [
-                        Div(text: 'Hello World ${id}'),
-                        Div('Ver mas resultados', onclick: $snapshot->nextPage()),
+                        $children,
+                        Div('Ver mas resultados', onclick: $snapshot->call('refresh')),
                     ];
                 },
+                paginate: true,
+                loading: Div('Loading...'),
+                empty: Div('No hay resultados'),
             ),
             Div('Hello World 2'),
             Div('Hello World 3'),
-            Div('Hello World 4'),
+            Div(
+                data: '{
+                    age: 30,
+                    name: "Byancode,
+                    sfgjhd() {
+                        console.log("sfgjhd");
+                    },
+                    get ageInDays() {
+                        return this.age * 365;
+                    },
+                }',
+                children: [
+                    Div('Hello World 4', ondblclick: 'shown = !shown'),
+                    Div('Hello World 5', show: 'shown'),
+                ],
+            ),
         ]),
         style: [
             'background-color' => 'red',
